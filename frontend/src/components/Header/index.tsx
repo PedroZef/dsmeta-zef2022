2e9bb30b7ffaa993150c2aca0e0adcc6fe2eb8f5
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2022-07-15 20:31:31 -0300

    Other components e Datepicker

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logo from '../../assets/img/logo.svg'
+
+import "./styles.css"
+
+
+function Header() {
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src={logo} alt="DSMeta" />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvolvido por
+          <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig & Pedro Zeferino da Silva</a>
+        </p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
