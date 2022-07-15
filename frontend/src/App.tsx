61f9c51c39c983fbb6d2961696295ad2d7699c99
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2022-07-15 16:48:41 -0300

    Project First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
-
+import NotificationButton from "./components/NotificationButton";
 
 function App() {
   return (
-    <h1>Olá Mundo!</h1>
-  )
+    <>
+      <h1>Olá Mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  );
 }
 
 export default App;
