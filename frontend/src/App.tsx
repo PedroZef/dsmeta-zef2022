7e50cb80a93d3d0eb26e69bfe8097bc9b981573f
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2022-07-16 14:52:38 -0300

    Project Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,20 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
-        <main>
-          <section id="sales">
-            <div className="dsmeta-container">
-              <SalesCard />
-            </div>
-          </section>
-        </main> 
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   );
 }
